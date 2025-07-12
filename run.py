97c3a6743e2724f966e8e9b534ca16214c14a199
suchokrates1 <dawsuchodolski@gmail.com>
2025-07-12 20:34:04 +0200

    Check alembic table before upgrading

--- run.py
+++ run.py
@@ -1,9 +1,13 @@
-from app import create_app
-from flask_migrate import upgrade
+from app import create_app, db
+from flask_migrate import upgrade, stamp
+from sqlalchemy import inspect
 
 app = create_app()
 
 if __name__ == "__main__":
     with app.app_context():
+        inspector = inspect(db.engine)
+        if "alembic_version" not in inspector.get_table_names():
+            stamp()  # mark current state
         upgrade()
     app.run(host="0.0.0.0", port=8000)
