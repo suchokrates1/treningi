979fde07d43d4a7bae987f19761aba37c652d296
suchokrates1 <dawsuchodolski@gmail.com>
2026-02-07 17:37:39 +0100

    UI/UX overhaul: glass header, toast notifications, mobile canvas menu, month tabs, scroll animations, empty states

--- tests/test_logout_button.py
+++ tests/test_logout_button.py
@@ -6,5 +6,5 @@ def test_logout_button_hidden_when_logged_out(client):
 def test_logout_button_visible_when_logged_in(client):
     with client.session_transaction() as sess:
         sess['admin_logged_in'] = True
-    response = client.get('/')
+    response = client.get('/admin/trainings')
     assert b'Wyloguj' in response.data
